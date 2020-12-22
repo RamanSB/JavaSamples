370ad1a7d7a86790b2bedfa1fb51cb917ee06dae
RamanSB <ramansb100@gmail.com>
2020-12-22 15:58:57 +0000

    Classic singleton pattern example

--- design-patterns-and-principles/src/ClassicSingleton.java
+++ design-patterns-and-principles/src/ClassicSingleton.java
@@ -8,22 +8,24 @@ C) A public getInstance() method returns the Singleton Object
 D) All instance variables are marked as private
  */
 
-public class SingletonPattern {
+public class ClassicSingleton {
 
 
     //A - constructor must be marked as private
-   private SingletonPattern(){};
+   private ClassicSingleton(){};
 
     //B - actual instance of the SingletonPattern
-   private static final SingletonPattern singleton = new SingletonPattern();
+   private static final ClassicSingleton singleton = new ClassicSingleton();
 
    //C - static (class-level) public getter
-    public static SingletonPattern getInstance(){
+    public static ClassicSingleton getInstance(){
         return singleton;
     }
 
     public static void main(String[] args){
-
+        ClassicSingleton instance1 = ClassicSingleton.getInstance();
+        ClassicSingleton instance2 = ClassicSingleton.getInstance();
+        System.out.println(instance1.equals(instance2)); //There is only 1 instance in memory at all times
     }
 
 }
