a93f3e6503bc1b345ac4a20b6444d6da0d19cc5c
RamanSB <ramansb100@gmail.com>
2021-01-17 20:10:53 +0000

    Completed Optional<> not OptionalInt

--- functional-programming/src/Optionals.java
+++ functional-programming/src/Optionals.java
@@ -6,7 +6,7 @@ import java.util.Optional;
  */
 public class Optionals {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         Optional<Double> optional = Optional.of(32.1);
         double value = optional.get();
         System.out.println(value);
@@ -17,11 +17,16 @@ public class Optionals {
         System.out.println(emptyOptional.or(()->Optional.of(10)).get());
         System.out.println(emptyOptional.orElse(15));
         emptyOptional.orElseGet(()->50);
-        emptyOptional.orElseThrow(); //Throws a no such element exception
-        emptyOptional.orElseThrow(()->new IllegalFormatPrecisionException(10)); //Can throw a random exception if optional is empty
-
+        if(false) { //set to true if needed
+            emptyOptional.orElseThrow(); //Throws a no such element exception
+            emptyOptional.orElseThrow(() -> new IllegalFormatPrecisionException(10)); //Can throw a random exception if optional is empty
+        }
         
-
+        //An optional that will return a value if a non-null value is present, otherwise throws NoSuchElementException
+        int random = (int) (Math.random() * 10);
+        System.out.println(random);
+        Optional<Integer> optionalInt = Optional.ofNullable(random%2==0 ? null : random);
+        optionalInt.orElseThrow(()->new Exception("Number " + random + " is even"));
     }
 
 
